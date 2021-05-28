ea3a541c8dd99d79c3c1480bb0bf4100b2ecd7dd
pramods444 <pramods@mkcl.org>
2021-05-28 18:00:54 +0530

    Middleware added but not used anywere

---
+++ middlewares/authenticate.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pramodshenkar/golang_todo_api/models"
+	"github.com/pramodshenkar/golang_todo_api/services"
+)
+
+func responseWithError(c *gin.Context, code int, message interface{}) {
+	c.AbortWithStatusJSON(code, gin.H{"message": message})
+}
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		requiredToken := c.Request.Header["Authorization"]
+		if len(requiredToken) == 0 {
+			responseWithError(c, 403, "Please login to your account")
+		}
+		userID, _ := services.DecodeToken(requiredToken[0])
+		result, err := new(models.UserModel).GetUserByEmail(userID)
+		if result.Email == "" {
+			responseWithError(c, 404, "User account not found")
+			return
+		}
+		if err != nil {
+			responseWithError(c, 500, "Something went wrong giving you access")
+			return
+		}
+		c.Set("User", result)
+		c.Next()
+	}
+}
